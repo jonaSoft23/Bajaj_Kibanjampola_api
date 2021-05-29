9aa395f96ffe75aa4783126476ed2e93c476873a
Owot jonathan <jonathanowot@gmail.com>
2021-05-29 05:04:56 +0300

    updated database config

--- config/database.js
+++ config/database.js
@@ -4,16 +4,10 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST'),
-        srv: env.bool('DATABASE_SRV', true),
-        port: env.int('DATABASE_PORT', 27017), //27017git
-        database: env('DATABASE_NAME', ),
-        username: env('DATABASE_USERNAME'),
-        password: env('DATABASE_PASSWORD'),
+        uri: env('DATABASE_URI'),
       },
       options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl: env.bool('DATABASE_SSL', true),
+        ssl: true,
       },
     },
   },
